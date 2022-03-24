92a9b3ef636f15f00229108f7b0e7c16efe8083a
A PONTEW <you@example.com>
2022-03-24 19:15:47 -0300

    teste3

---
+++ app.js
@@ -0,0 +1,23 @@
+function calcular1(){
+
+    let res =document.getElementById("resultado1")
+  res.innerHTML =`<p> clicou no Primeiro botão  <p/> `
+}
+
+function calcular2(){
+
+    let res =document.getElementById("resultado2")
+  res.innerHTML =`<p> clicou no segundo botão  <p/> `
+}
+
+function calcular3(){
+
+    let res =document.getElementById("resultado3")
+  res.innerHTML =`<p> clicou no terceiro botão  <p/> `
+}
+
+function calcular4(){
+
+    let res =document.getElementById("resultado4")
+  res.innerHTML =`<p> clicou no quarto botão  <p/> `
+}
